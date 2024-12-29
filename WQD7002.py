78ec9f19d9e4d568240f824bf617bfde91e0673e
VinodAnnukaran <130421078+VinodAnnukaran@users.noreply.github.com>
2024-12-29 23:41:01 +0800

    Update WQD7002.py

--- WQD7002.py
+++ WQD7002.py
@@ -4,24 +4,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import seaborn as sns
-import matplotlib.pyplot as plt
-import re
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-from nltk.stem import WordNetLemmatizer
-from textblob import TextBlob
-from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-from nltk import download
-from wordcloud import WordCloud
-from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.svm import SVC
-from sklearn.metrics import classification_report
-
-# Download necessary NLTK resources
-download('punkt')
-download('stopwords')
-download('wordnet')
 
 # Initialize lemmatizer and VADER analyzer
 lemmatizer = WordNetLemmatizer()
