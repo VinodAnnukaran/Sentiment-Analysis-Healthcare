4244bdbcb324bb999ad672d6cadf98d6636f18b5
VinodAnnukaran <130421078+VinodAnnukaran@users.noreply.github.com>
2025-01-06 15:30:07 +0800

    Update sentimentproject.py

--- sentimentproject.py
+++ sentimentproject.py
@@ -370,8 +370,8 @@ elif selected_tab == "Dataset Overview":
                         st.session_state.data_hc.apply(refine_sentiment, axis=1)
                     )
         
-                else:
-                    st.error("The uploaded file must contain the column 'HCAHPS Answer Description'.")
+            if __name__ == "__main__":
+                main
         
     
 ###############################################    
